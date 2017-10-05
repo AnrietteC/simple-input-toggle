34c78fb87c975a986f03c66bcf50d00810bfde8d
Anriëtte Myburgh <anriette@yonder.co.za>
2017-10-05 18:07:34 +0200

    Update scripting

--- js/jquery.toggleinput.js
+++ js/jquery.toggleinput.js
@@ -5,16 +5,27 @@ jQuery.fn.toggleInput = function () {
 	_container.addClass('has-toggle-input');
 	_container.find( 'input' ).hide();
 
-	$('label[for="'+ _container.find('input:checked').attr('id') +'"]').addClass('active');
+	_initLabel = $('label[for="'+ _container.find('input:checked').attr('id') +'"]');
+	if (!_initLabel.length) {
+		_initLabel = _container.find('input:checked').parent('label');
+	}
+	_initLabel.addClass('active');
 
 	this.on( 'click', 'label', function ( e ) {
+		e.preventDefault();
+
 		// Add active class to labels
 		_container.find('label.active').removeClass('active');
 		var _label = $(e.currentTarget);
 		_label.addClass('active');
 
 		// Add checked state to inputs
-		var _input     = $( '#' + e.currentTarget.htmlFor );
+		var _input = $( "#" + _label.htmlFor );
+
+		if (!_input.length) {
+			_input = _label.find('input');
+		}
+
 		$( 'input[name="' + _input.attr( 'name' ) + '"]:checked' ).prop( 'checked', false ).removeAttr( 'checked' );
 		_input.prop( 'checked', true ).attr( 'checked', 'checked' );
 	} );
